b6b2184ddc22e139abfa55bed863a62b3cd7cb2c
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-10-20 11:58:06 -0600

    first start

--- CreditCardP/src/cardIntro.java
+++ CreditCardP/src/cardIntro.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 
 public class cardIntro
 	{
+		public static void main(String[] args) throws IOException
+			{
+				importCredit();
+				
+				
+			}
 
+		public static void importCredit() throws IOException
+		{
+			
+			Scanner myCredit = new Scanner(new File("CreditCardBoi.txt"));
+			
+			while(myCredit.hasNext())
+				{
+					String letter = myCredit.nextLine();
+					System.out.println(letter);
+			
+			
+			
+	        	}
+		
+		
+		
+		
+		
+	    }
 	}
